d46b5f41e7d7aee5ca9db7da5fcf39cf18e0752b
neutrino <jrusev@gmail.com>
2014-02-19 14:27:18 +0200

    fixed keyboardstate bug

--- CastleWolfenstein/Wolfenstein.Common/KeyboardState.cs
+++ CastleWolfenstein/Wolfenstein.Common/KeyboardState.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Wolfenstein.Common
@@ -12,23 +9,24 @@ namespace Wolfenstein.Common
     /// </summary>
     public class KeyboardState
     {
-        private bool keyIsDown;
-        private Keys keyPressed;
+        private List<Keys> pressedKeyList;
 
         public KeyboardState()
         {
-            this.keyIsDown = false;
+            this.pressedKeyList = new List<Keys>();
         }
 
         public void OnKeyDown(Keys key)
         {
-            this.keyIsDown = true;
-            this.keyPressed = key;
+            if (!this.pressedKeyList.Contains(key))
+            {
+                this.pressedKeyList.Add(key);
+            }
         }
 
         public void OnKeyUp(Keys key)
         {
-            this.keyIsDown = false;
+            this.pressedKeyList.Remove(key);
         }
 
         /// <summary>
@@ -37,7 +35,7 @@ namespace Wolfenstein.Common
         /// <param name="key">Enumerated value that specifies the key to query.</param>
         public bool IsKeyDown(Keys key)
         {
-            return this.keyIsDown == true && this.keyPressed == key;
+            return this.pressedKeyList.Contains(key);
         }
     }
 }
